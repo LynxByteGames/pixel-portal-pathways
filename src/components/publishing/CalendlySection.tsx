2dbd4fe4b10170cd8c41c7bf18f427ad41ad2401
LYNXBYTE GAMES <lynxbytegames@gmail.com>
2025-05-26 17:48:25 +0200

    Update CalendlySection.tsx

--- src/components/publishing/CalendlySection.tsx
+++ src/components/publishing/CalendlySection.tsx
@@ -21,7 +21,7 @@ const CalendlySection = () => {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <a
-  href="https://drive.google.com/file/d/1D_RuXG_nvnZpPiBqGJaVn-71VjOVI1Ma/view?usp=sharing"
+  href="https://drive.google.com/file/d/1k-75qs4_n_C5F5pPMkXOOyApfY9MQ4HB/view?usp=sharing"
   target="_blank"
   rel="noopener noreferrer"
   className="inline-block bg-gradient-to-r from-purple-primary to-purple-accent hover:opacity-90 text-white font-semibold text-lg px-8 py-6 h-auto rounded-lg"
